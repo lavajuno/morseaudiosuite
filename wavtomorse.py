9ed2b251e85c17c16762caeb885f0bc9c436183c
Juno <jvmeifert@gmail.com>
2021-10-01 17:34:47 -0400

    Better comments in wavtomorse.py

--- wavtomorse.py
+++ wavtomorse.py
@@ -1,18 +1,23 @@
 import wave
 import struct
 
-# Time in samples (assuming 48000hz sample rate -- best for morse)
-# 0.1s: 4800, 0.05s: 2400, etc.
+# Morse unit time in samples (assuming 48000hz sample rate in input file)
+# 0.10s: 4800 (Slowest, most reliable)
+# 0.05s: 2400 (Default - Best for NFM transmission)
+# 0.02s: 960
+# 0.01s: 480 (Fastest, least reliable)
 UNIT_TIME = 2400
 
-# Value at which an amplitude is considered ON or OFF. (10000-16000) Default value is 13000
+# Value at which an amplitude is considered ON or OFF. (10000-16000) Default value is 13000.
+# Noisier input may require higher values. However, if the value is too high, dits and dahs might be rejected as noise.
 AMPLITUDE_LOGIC_THRESHOLD = 13000
 
 # Offset (in samples) to apply to start decoding.
 # This is necessary because (ideally) the start listener won't trip until midway up the first wave.
-# It shouldn't be necessary to change this unless the frequency of the input code is changed. (Default: 550 Hz)
+# It shouldn't be necessary to change this unless the frequency of the input code is changed. (Default: 550 Hz: -8)
 START_DECODE_OFFSET = -8
 
+# Morse decoding chart to use. Included is standard international morse (A-Z, 0-9, spaces)
 MORSE_ENCODE_CHART={'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
@@ -27,23 +32,25 @@ MORSE_ENCODE_CHART={'A':'.-', 'B':'-...',
                     '7':'--...', '8':'---..', '9':'----.',
                     '0':'-----', ' ':'/'}
 
+# An inverted version of the decoding chart that is more useful for encoding. Shouldn't need to be changed.
 MORSE_DECODE_CHART = {a: b for b, a in MORSE_ENCODE_CHART.items()}
 
-# Decode a deviation value to logical ON or OFF
+# Decide if a certain deviation is considered logically ON or OFF.
 def logicalDecode(deviation):
     if(deviation > AMPLITUDE_LOGIC_THRESHOLD):
         return 1
     else:
         return 0
 
-# Find the average absolute deviation from zero of a chunk. Should be half the amplitude of the wave.
+# Find the average absolute deviation of the amplitude of a chunk of audio.
 def avgAbsDeviation(chunk):
     totFrames = 0
     for frame in chunk:
         totFrames += abs(frame)
     return totFrames / len(chunk)
 
-# Find the start of the morse code data in a stream using the amplitude
+# Find the start of the morse code data in a stream by detecting the first logical ON
+# Make sure your squelch is ON to prevent noise from tripping this block.
 def findStart(chunk):
     iterIndex = 0
     while(abs(chunk[iterIndex]) < AMPLITUDE_LOGIC_THRESHOLD):
@@ -54,6 +61,9 @@ def findStart(chunk):
     return iterIndex
 
 # Get raw morse data from a wav file - the heart of the program
+# Splits a file into its individual frames, arranges them into chunks,
+# analyzes the average absolute deviation of each chunk, and outputs
+# the resulting digital data.
 def getMorseData(filename):
     with wave.open(filename, "r") as f:
         sampleRate = f.getframerate()
@@ -83,7 +93,10 @@ def getMorseData(filename):
         return decodedChunks
         
 
-# Convert raw morse into human readable dits, dahs, and slashes.
+# Convert raw morse data into human readable dits, dahs, and slashes.
+# International morse defines a dit as one time unit and a dah as three time units.
+# It also defines the space between characters as three units of silence, and the
+# space between words as seven units of silence.
 def getHumanReadableMorse(morseData):
     humanMorse = ""
     morseIter = 0
@@ -117,7 +130,7 @@ def getHumanReadableMorse(morseData):
         morseIter += 1
     return humanMorse
 
-#Decode human readable morse into letters, numbers, and spaces.
+# Decode human readable morse into letters, numbers, and spaces.
 def decodeHumanReadableMorse(message):
     words = message.split("/")
     i = 0
